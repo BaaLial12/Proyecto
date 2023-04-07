a026e97dccca8aca45a911c3c198868a486a73e1
David Ureña <bluberyasp161@gmail.com>
2023-04-07 17:17:19 +0200

    primeros diseños chorras xd

--- resources/views/marketplace/index.blade.php
+++ resources/views/marketplace/index.blade.php
@@ -3,15 +3,15 @@
 <div class="owl-carousel owl-theme mx-auto"> <!-- Agregar clase "mx-auto" -->
     @foreach ($categorias as $categoria)
         <div class="card text-center mb-4 mx-auto"> <!-- Agregar clase "text-center" y "mx-auto" -->
-            <div class="card-header"><strong>{{ $categoria->nombre }}</strong></div>
-            <div class="card-body text-center">
+            <div class="card-header" style="background-color: violet"><strong>{{ $categoria->nombre }}</strong></div>
+            <div class="card-body text-center" style="background-color: red">
                 <div class="row">
                     @foreach ($plataformas->where('category_id', $categoria->id) as $plataforma)
-                        <div class="col-md-4">
-                            <div class="btn btn card mb-4">
-                                <div class="card-body">
+                        <div class="col-md-4" >
+                            <div class="btn btn card mb-4" style="background-color: brown">
+                                <div class="card-body" >
                                     <h5 class="card-title"><strong>{{ $plataforma->nombre }}</strong></h5>
-                                    <p class="card-text">Apartir de : {{round($plataforma->suscripcion/$plataforma->capacidad, 2) }}€</p>
+                                    <p class="card-text" style="margin-top: 30px">Apartir de : {{round($plataforma->suscripcion/$plataforma->capacidad, 2) }}€</p>
                                 </div>
                             </div>
                         </div>
