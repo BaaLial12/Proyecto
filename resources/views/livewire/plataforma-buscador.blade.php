1db16c5773c80f72fc59a734cb4681d1030a9f1e
David Ureña <bluberyasp161@gmail.com>
2023-04-12 20:38:07 +0200

    unos cuantos estilos al buscador de crear grupo

--- resources/views/livewire/plataforma-buscador.blade.php
+++ resources/views/livewire/plataforma-buscador.blade.php
@@ -1,13 +1,17 @@
 <div>
     {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
+    <div class="search-container position-relative pt-10">
+        <input type="text" wire:model="search" placeholder="Buscar plataforma..." class="search-input form-control rounded ">
+        @if ($search !== '')
+          <ul class="platforms-list list-group position-absolute w-100 mt-1 ">
+            @foreach ($platforms as $platform)
+              <li class="platform-item list-group-item-action list-group-item-danger cursor-pointer">{{ $platform->nombre }}</li>
+            @endforeach
+          </ul>
+        @endif
+      </div>
 
 
-    <input type="text" wire:model="search" placeholder="Buscar plataforma..." class="">
-    <ul>
-        @foreach ($platforms as $platform)
-            <li>{{ $platform->nombre }}</li>
-        @endforeach
-    </ul>
 
 
 
