298ad510f290d9f38140bb623e6344d28d432426
David Ureña <bluberyasp161@gmail.com>
2023-04-19 20:08:07 +0200

    Actualizar las credenciales , terminado

--- app/Http/Controllers/CredentialController.php
+++ app/Http/Controllers/CredentialController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Credential;
 use App\Models\Group;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 
 class CredentialController extends Controller
 {
@@ -12,19 +14,35 @@ class CredentialController extends Controller
 
 
 
-    public function update(Request $request){
+    public function update(Request $request)
+    {
+        //Primero lo que hacemos es obtener el modelo de Group que corresponde al ID que hemos mandado
+        //Basicamente traernos toda la informacion del grupo
+        $grupo = Group::find($request->grupo);
 
-        // dd($request->grupo);
+        //Me guardo el id del usuario
+        $user = Auth::user()->id;
 
-        //TODO: YA ESTA , AHORA HACER VALIDACIONES
-        $grupo = Group::findOrFail($request->grupo);
+        //Me guardo el id del usuario al que esta asignado el grupo
+        $group_id = $grupo->user_id;
+
+        //Si el id del usuario logueado es distinto al usuario propietario que recargue
+        if(!$user == $group_id){
+            return Redirect::back();
+        }
+        //Luego nos traemos todos los datos que estan asociados a las credenciales a traves de la relacion que hay en group
+        $credential = $grupo->credential;
+
+        //Actualizamos los campos de credential con lo que mandamos por los campos
+        $credential->email = $request->email;
+        $credential->password = $request->password;
 
 
-        dd($grupo->credential_id);
+        //Actualizamos
+        $credential->update();
 
 
+        //Y recargamos la pagina
+        return Redirect::back()->with('success_msg', 'Credenciales Actualizada');
     }
-
-
-
 }
