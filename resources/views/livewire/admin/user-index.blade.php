b7632d5e4851c05577869ad7b031fcc59d5c7673
David Ureña <bluberyasp161@gmail.com>
2023-04-20 14:20:15 +0200

    actualizacion diseño tabla

--- resources/views/livewire/admin/user-index.blade.php
+++ resources/views/livewire/admin/user-index.blade.php
@@ -6,7 +6,8 @@
 
 
         <div class="card-header">
-            <input type="search" wire:model="busqueda" class="form-control" placeholder="Ingrese el nombre o correo de un usuario">
+            <input type="search" wire:model="busqueda" class="form-control"
+                placeholder="Ingrese el nombre o correo de un usuario">
         </div>
 
 
@@ -14,47 +15,66 @@
 
 
 
-        <div class="card-body">
-            <div class="table-responsive">
-                <table class="table">
-                    <thead>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead>
 
-                            <th>ID</th>
-                            <th>Nombre</th>
-                            <th>Email</th>
-                            <th></th>
+                            <tr class="bg-gradient-gray-dark text-white text-sm leading-normal">
+                                <th class="py-3 px-6 text-left">ID</th>
+                                <th class="py-3 px-6 text-left">Nombre</th>
+                                <th class="py-3 px-6 text-left">Email</th>
+                                <th class="py-3 px-6 text-left">Ultima vez</th>
+                                <th class="py-3 px-6 text-center">Status</th>
+                                <th class="py-3 px-6 text-center">Acciones</th>
+                            </tr>
 
-                    </thead>
+                        </thead>
 
-                    <tbody>
-                        @foreach ($users as $user )
-                                <tr>
-                                    <td>{{$user->id}}</td>
-                                    <td>{{$user->name}}</td>
-                                    <td>{{$user->email}}</td>
-                                    <td style="width: 2vw">
-                                        <a class="btn btn-primary" href="{{route('admin.users.edit' , $user)}}">Editar</a>
+                        <tbody class="text-gray-600 font-light">
+                            @foreach ($users as $user)
+                                <tr class="border-b border-gray-200 hover:bg-gray-100">
+                                    <td class="py-3 px-6">{{ $user->id }}</td>
+                                    <td class="py-3 px-6">{{ $user->name }}</td>
+                                    <td class="py-3 px-6">{{ $user->email }}</td>
+                                    <td class="py-3 px-6">{{ Carbon\Carbon::parse($user->last_seen)->diffForHumans() }}
+                                    </td>
+                                    <td class="py-3 px-6 text-center">
+
+                                        @if ($user->last_seen >= now()->subSeconds(30))
+                                            <span class="badge badge-pill badge-success py-1 px-3  text-lg">
+                                                Online
+                                            </span>
+                                        @else
+                                            <span class="badge badge-pill badge-danger py-1 px-3 text-lg">
+                                                Offline
+                                            </span>
+                                        @endif
+
+
+                                    </td>
+                                    <td class="py-3 px-6 text-center" style="width: 2vw" >
+                                        <a class="btn btn-primary"
+                                            href="{{ route('admin.users.edit', $user) }}">Editar</a>
                                     </td>
                                 </tr>
-                        @endforeach
+                            @endforeach
 
-                    </tbody>
-
-
-                </table>
-              </div>
-        </div>
+                        </tbody>
 
 
-        <div class="card-footer">
-            {{$users->links()}}
-        </div>
+                    </table>
+                </div>
+            </div>
 
+
+            <div class="card-footer">
+                {{ $users->links() }}
+            </div>
         @else
-
-        <div class="card-body">
-            <strong>NO HAY NINGUN REGISTRO</strong>
-        </div>
+            <div class="card-body">
+                <strong>NO HAY NINGUN REGISTRO</strong>
+            </div>
 
         @endif
 
