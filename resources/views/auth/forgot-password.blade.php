478b25b4b67bc85dddf074a02e78c86aba881aef
David Ureña <bluberyasp161@gmail.com>
2023-04-26 17:28:44 +0200

    diseño forgot-password

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,34 +1,39 @@
-<x-guest-layout>
-    <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
+<!DOCTYPE html>
+<html lang="es">
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+<head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/x-icon" href="/assets/logo-vt.svg" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Forgot Password</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous" />
+</head>
+
+<body class="bg-info d-flex justify-content-center align-items-center vh-100">
+    <div class="bg-white p-5 rounded-5 text-secondary shadow" style="width: 25rem">
+        <div class="d-flex justify-content-center">
+            <img src="{{ Storage::url('img/logo/Netflix.png') }}" alt="login-icon" style="height: 7rem" />
         </div>
-
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
-            </div>
-        @endif
-
-        <x-validation-errors class="mb-4" />
-
+        <div class="text-center fs-1 fw-bold">Forgot Password</div>
         <form method="POST" action="{{ route('password.email') }}">
             @csrf
-
-            <div class="block">
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <div class="mt-4">
+                <label for="email" class="form-label">Email</label>
+                <input class="form-control bg-light" type="email" name="email" :value="old('email')" required
+                    autofocus autocomplete="email" />
             </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Email Password Reset Link') }}
-                </x-button>
-            </div>
+            <button type="submit" class="btn btn text-white w-100 mt-4 fw-semibold shadow-sm"
+                style="background-color:  #00CDD0">
+                {{ __('Send Password Reset Link') }}
+            </button>
         </form>
-    </x-authentication-card>
-</x-guest-layout>
+        <div class="d-flex gap-1 justify-content-center mt-1">
+            <div>Remember your password?</div>
+            <a href="{{ route('login') }}" class="text-decoration-none text-info fw-semibold">Log in</a>
+        </div>
+    </div>
+</body>
+
+</html>
