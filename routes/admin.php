9f1072f9719f5d5051373280d6e52fee267081ba
David Ureña <bluberyasp161@gmail.com>
2023-04-27 12:33:38 +0200

    cambio de logica en rutas por API stripe

--- routes/admin.php
+++ routes/admin.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PlataformController;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\ServiceController;
+use App\Models\Plataform;
 
 Route::get('' , [HomeController::class , 'index'])->middleware('can:admin.home')->name('admin.home');
 
@@ -20,8 +21,26 @@ Route::resource('users', UserController::class)->only(['index' , 'edit' , 'updat
 Route::resource('categories' , CategoryController::class)->names('admin.categories');
 
 
-//Creamos la ruta resource para poder hacer el CRUD de Plataformas
+//Creamos la ruta resource para poder hacer el CRUD de Plataformas como MENTAR PROBLEMA CON API
 
-Route::resource('plataforms' , PlataformController::class)->names('admin.plataforms');
+// Route::resource('plataforms' , PlataformController::class)->names('admin.plataforms');
+
+Route::get('plataforms' , [PlataformController::class , 'index'])->name('admin.plataforms.index');
+
+Route::get('plataforms/create' , [PlataformController::class , 'create'])->name('admin.plataforms.create');
+
+Route::post('plataforms' , [PlataformController::class , 'store'])->name('admin.plataforms.store');
+
+
+Route::get('plataforms/{nombre}/edit' , [PlataformController::class , 'edit'])->name('admin.plataforms.edit');
+
+Route::get('plataforms/{nombre}/update' , [PlataformController::class , 'update'])->name('admin.plataforms.update');
+
+
+Route::delete('plataforms/{nombre}' , [PlataformController::class , 'destroy'])->name('admin.plataforms.destroy');
+
+
+
+
 
 Route::resource('services', ServiceController::class)->names('admin.servicess');
