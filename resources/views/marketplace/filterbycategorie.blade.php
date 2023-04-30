7d451703db3103aa1a3387f9e27dd3081fe852ae
David Ureña <bluberyasp161@gmail.com>
2023-04-30 20:38:15 +0200

    diseño?

--- resources/views/marketplace/filterbycategorie.blade.php
+++ resources/views/marketplace/filterbycategorie.blade.php
@@ -1,27 +1,39 @@
 <x-app-layout>
 
     <div class="container">
-        <div class="card text-center mb-4 mx-auto mt-3">
-            <div class="row">
-                @foreach ($plataforms_by_categorie as $plataforma)
-                    @if ($plataforma->category_id == $categoria_id)
-                    <div class="col-lg-3 col-md-4 col-sm-12 mb-4">
-                        <div class="card mb-4" style="background-color: #D8C3FF">
-                            <div class="card-body" >
-                                <h5 class="card-title" style="font-size: 1.5rem"><strong>{{ $plataforma->nombre }}</strong></h5>
-                                <p class="card-text" style="margin-top: 30px; font-size: 1.2rem">A partir de: {{round($plataforma->suscripcion/$plataforma->capacidad, 2) }}€</p>
-                                <a href="{{route('groups.showGroups' , $plataforma->id )}}" class="btn btn-outline-dark" style="background-color: #00CDD0">Ver grupos</a>
+        <div class="owl-carousel owl-theme mx-auto">
+            <div class="card text-center mb-4 mx-auto mt-3">
+                <div class="row">
+                    @foreach ($plataforms_by_categorie as $plataforma)
+                        @if ($plataforma->category_id == $categoria_id)
+                            <div class="col-md-4">
+                                <div class="card mb-4" style="background-color: #D8C3FF">
+                                    <div class="card-body">
+                                        <h5 class="card-title" style="font-size: 1.5rem">
+                                            <strong>{{ $plataforma->nombre }}</strong>
+                                        </h5>
+                                        <p class="card-text" style="margin-top: 30px; font-size: 1.2rem">
+                                            Apartir de :
+                                            {{ round($plataforma->suscripcion / $plataforma->capacidad, 2) }}€
+                                        </p>
+                                        <a href="{{ route('groups.showGroups', $plataforma->nombre) }}"
+                                            class="btn btn-outline-dark" style="background-color: #00CDD0">Ver
+                                            grupos</a>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    @endif
-                @endforeach
+                        @endif
+                    @endforeach
+                </div>
             </div>
         </div>
     </div>
 
 
-    {{--TODO: FALTARIA PONERLO BONITO CON SU CARD(QUE SERA IGUAL ) --}}
+
+
+
+    {{-- TODO: FALTARIA PONERLO BONITO CON SU CARD(QUE SERA IGUAL ) --}}
 
 
 
