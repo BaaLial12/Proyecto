8adc44c7e296b9cc0235905980122e54400ea2b8
David Ureña <bluberyasp161@gmail.com>
2023-05-03 17:43:11 +0200

    añadido shadow y cambio de colores

--- resources/views/livewire/search-plataforms.blade.php
+++ resources/views/livewire/search-plataforms.blade.php
@@ -16,11 +16,11 @@
         <input type="text" wire:model="search" placeholder="Buscar plataforma..."
             class="search-input form-control rounded ">
         @if ($search != '')
-            <div class="card text-center mb-4 mx-auto mt-3">
+            <div class="card text-center mb-4 mx-auto mt-3 shadow-lg border-0">
                 <div class="row">
                     @foreach ($plataforms as $plataforma)
                         <div class="col-lg-3 col-md-4 col-sm-12">
-                            <div class="card mb-4" style="background-color: #D8C3FF">
+                            <div class="card mb-4" style="background-color: #f0f0f0">
                                 <div class="card-body">
                                     <h5 class="card-title" style="font-size: 1.5rem">
                                         <strong>{{ $plataforma->nombre }}</strong>
@@ -30,7 +30,7 @@
                                         {{ round($plataforma->suscripcion / $plataforma->capacidad, 2) }}€
                                     </p>
                                     <a href="{{ route('groups.showGroups', $plataforma->id) }}"
-                                        class="btn btn-outline-dark" style="background-color: #00CDD0">Ver
+                                        class="btn btn-outline-dark" style="background-color: #72C3DC">Ver
                                         grupos</a>
                                 </div>
                             </div>
