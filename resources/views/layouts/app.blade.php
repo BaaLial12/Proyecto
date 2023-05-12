7efbc6e171b9b0d67028f8cce6af6e01d00d8a6d
david <bluberyasp161@gmail.com>
2023-05-12 10:26:24 +0000

    img creative common se habia vuelto loco

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -155,7 +155,7 @@
             <div class="row">
                 <div class="col-12 text-center">
                     <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">
-                        <img alt="Licencia de Creative Commons" class="mx-auto mt-3" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" />
+                        <img alt="Licencia de Creative Commons" class="mx-auto mt-3 img-fluid" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" />
                     </a>
 
                     <br />Este obra está bajo una <a rel="license" class="text-white" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">licencia de Creative Commons Reconocimiento-NoComercial-SinObraDerivada 4.0 Internacional</a>.
